d7fcb4ec1bfe50c93acd5a922a6d3ee7cb2f3545
Thong Do <tdo05824@gmail.com>
2019-12-24 00:51:54 +0700

    new experiment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: this.props.value
   };
 
   handleIncrement = () => {
@@ -19,6 +19,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={this.props.onDelete}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
